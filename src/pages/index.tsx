c6107a7392deabd99e47b19840823894e6ae8186
Marco Azzurrini <marcoazzurrini@Marcos-MacBook-Pro.local>
2023-10-04 14:56:43 +0200

    add shacn form and input components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,7 @@ export default function Home() {
         <meta name="description" content="Simple Breakeven ROAS Calculator" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        hello
-      </main>
+      <main className="">hello</main>
     </>
   );
 }
